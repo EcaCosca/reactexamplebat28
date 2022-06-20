98d2817ab13cb74360d51a6633cd8dda82e036a0
Eca Coscarelli <ecacoscarelli@ARCA.local>
2022-06-20 17:22:27 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import MySecondComponent from './MySecondComponent';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Test example for React v.18</h1>
+        <MySecondComponent prop1={"Shutle"} prop2={"trip"}/>
+        <MySecondComponent prop1={"PROP 1"} prop2={"PROP 2"}/>
+
+        <Button name={"Ali"} />
+        <Button name={"Darshana"} />
       </header>
     </div>
   );
---
+++ src/MySecondComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MySecondComponent({prop1, prop2}){
+ return (
+   <div className="MySecondComponent">
+     Props are {prop1} and {prop2}
+   </div>
+ );
+}
